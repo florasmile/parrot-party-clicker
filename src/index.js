44a5fb537f83c293440de3b2486bdb269d8fce8e
dehuihu <dehuihu@gmail.com>
2025-05-22 12:23:33 -0700

    Finished updating count and adding reset button, and change img size when reaching a specific count

--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
 // Add javascript code to Party Parrot Clicker such that:
+const NUM_OF_COUNTS = 10;
 
+const state = {
+  clickCountLabel: null,
+  clickCount: 0,
+  parrotImg: null,
+  resetButton: null
+};
+
+const handleImgClicked = () => {
+  //add count to state
+  state.clickCount++;
+  state.clickCountLabel.textContent = state.clickCount;
+  if (state.clickCount === NUM_OF_COUNTS) {
+    state.parrotImg.classList.add('big-parrot');
+  } else {
+    state.parrotImg.classList.remove('big-parrot');
+  }
+  //change count in html
+};
+
+const handleResetCount = () => {
+  state.clickCount = 0;
+  state.clickCountLabel.textContent = state.clickCount;
+};
+
+const registerEvents = () => {
+  state.parrotImg.addEventListener('click', handleImgClicked);
+  state.resetButton.addEventListener('click', handleResetCount);
+};
+const loadControls = () => {
+  state.clickCountLabel = document.getElementById('click-count-label');
+  state.parrotImg = document.querySelector('img');
+  state.resetButton = document.getElementById('reset-button');
+};
+
+const onLoaded = () => {
+  loadControls();
+  registerEvents();
+};
+
+onLoaded();
 // 1. A click on the Party Parrot increases the counter by 1
 // 2. Add a button called ‘reset’ that will reset the
 //    click count to ‘0’.
